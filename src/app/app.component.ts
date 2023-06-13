b353ea62d3541e1a5a668696e99567607cd17ded
JessicaYolanda07 <jessiy.condoris@gmail.com>
2023-06-13 19:56:15 -0400

    segundo proyecto de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto2p';
+  nombre = 'Jessica condori';
+  textPlaceHolder='Escriba su nombre aquí ...';
+  deshabilitado= true;
+  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
+  constructor()
+  {
+    setInterval(()=>this.deshabilitado = false, 3000)
+  }
+  getSuma(num: number, num2:number){
+    return num+num2;
+  }
 }
+
